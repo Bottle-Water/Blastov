967242b37c383efa93815754834f7bf9fab4e18f
oflaig <oliver.flaig@gmail.com>
2026-02-03 23:04:53 +0100

    Added basic functionality

--- src/music/harmony.py
+++ src/music/harmony.py
@@ -9,6 +9,9 @@ CHORD_INTERVALS = {
     'minor': [0, 3, 7, 12],      # Minor triad + octave
     'dim': [0, 3, 6, 12],        # Diminished triad + octave
     'dom7': [0, 4, 7, 10],       # Dominant 7th
+    'maj7': [0, 4, 7, 11],       # Major 7th
+    'min7': [0, 3, 7, 10],       # Minor 7th 
+    'dim7': [0, 3, 6, 9]         # Diminished chord
 }
 
 # Scale degree (1-7) to semitone offset in major scale
@@ -58,3 +61,47 @@ def get_dominant_planet(sat: Satellite, planets: List[Planet]) -> Planet:
             max_weight = weight
             dominant = p
     return dominant
+
+def get_planet_weights(sat: Satellite, planets: List[Planet]) -> Planet:
+    """Returns a list of weights of all planets, normalised to 0-1"""
+    weights = []
+    total_weight = 0
+    for p in planets:
+        dist = np.linalg.norm(p.pos - sat.pos)
+        weight = p.mass / (dist + 1.0)
+        weights.append(weight)
+        total_weight += weight
+
+    weights = [val/total_weight for val in weights]
+    return weights
+
+def get_interval_velocities(planets: List[Planet], weights: List) -> List:
+    velocities = np.zeros(12)
+
+    # It feels wrong to have weights and planets linked by index and nothing
+    # else! But idk a better way
+    for i, p in enumerate(planets):
+        # Also this is copied from that function up there! I guess this can 
+        # be rearranged
+        semitone_offset = SCALE_DEGREE_TO_SEMITONE.get(p.chord_root, 0)
+        root_note = (Config.KEY + semitone_offset) % 12
+        intervals = CHORD_INTERVALS.get(p.quality, CHORD_INTERVALS['major']) # maj as default
+        intervals = [(root_note + interval) % 12 for interval in intervals]
+        
+        for interval in intervals:
+            if velocities[interval] == 0:
+                velocities[interval] = weights[i]
+            else:
+                velocities[interval] = max(velocities[interval], weights[i])
+    
+    # Sorry - I'm learning to use list comprehension lol 
+    velocities = [max(pow(vel, 2)-0.1, 0) for vel in velocities]
+    velocities = [int(vel * 127) for vel in velocities]
+    return velocities
+
+def get_notes() -> List:
+    """
+    Returns chromatic notes in desired octave
+    """
+    base_note = (Config.BASE_OCTAVE * 12) + Config.KEY
+    return np.arange(12) + base_note
